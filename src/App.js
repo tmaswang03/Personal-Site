c1cb88050b2a6326107127b96d3cf312c3eea3ce
tmaswang69 <thomaswang003@gmail.com>
2021-08-20 11:10:09 -0400

    play4d around with stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar'; 
+import Textbox from './components/Textbox';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar /> 
+      <Textbox />
     </div>
   );
 }
---
+++ src/components/Textbox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Textbox.css'
+const Textbox = () => {
+    return (
+        <div className = "textbox">
+            <h3 className = "text">Wassup my name's toto and i wuv soso</h3>
+        </div>
+    )
+}
+
+export default Textbox
